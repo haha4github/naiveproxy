a9e547bd40033b347c4d11b443009c0510d600b0
haha4github <55265840+haha4github@users.noreply.github.com>
2023-02-12 16:42:03 +0800

    fix

--- src/build.sh
+++ src/build.sh
@@ -79,4 +79,4 @@ export DEPOT_TOOLS_WIN_TOOLCHAIN=0
 
 ./gn/out/gn gen "$out" --args="$flags $EXTRA_FLAGS" --script-executable=$PYTHON
 
-ninja -C "$out" naive libnaive sharedlibnaive
+ninja -C "$out" naive libnaive libmyc++ libmyc++abi
